eeeb39e064cddd83987936caa8ba70d257844733
OneGoodReas0n <korobovi4@gmail.com>
2019-07-28 22:45:31 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
+
+import { Route } from "react-router-dom";
 import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
 
 export default () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route path="/" exact render={() => <Home />} />
+    </div>
+  );
 };
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./home.scss";
+
+export default () => {
+  return <div></div>;
+};
