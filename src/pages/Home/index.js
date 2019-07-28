2375704641ec1872a648e41f6d36b55cf6ec5d32
OneGoodReas0n <korobovi4@gmail.com>
2019-07-28 23:51:09 +0200

    Implementing background, decor-elements

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-
 import "./home.scss";
 
 export default () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="decor left-position"></div>
+      <div className="decor right-position"></div>
+    </div>
+  );
 };
